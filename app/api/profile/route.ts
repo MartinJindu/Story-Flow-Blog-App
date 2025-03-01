254d41643841147d869cfdc81e00399d2189903d
MartinJindu <martinchijindu@gmail.com>
2025-03-02 00:03:41 +0100

    fix authOptions import

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/option";
 import { PrismaClient } from "@prisma/client";
 import axios from "axios";
 import crypto from "crypto";
